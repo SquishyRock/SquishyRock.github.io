5a3f78594ffe957215186811c4974105b1034546
Squishy <iacampoaw@gmail.com>
2019-04-17 15:39:43 -0400

    Updates

---
+++ precache-manifest.2373e62af71ec063d1d096149660030a.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = [
+  {
+    "revision": "70b98d0682604a542615",
+    "url": "/static/js/main.4061b497.chunk.js"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.c5541365.js"
+  },
+  {
+    "revision": "ed158554fdd93e792e13",
+    "url": "/static/js/2.b0926c85.chunk.js"
+  },
+  {
+    "revision": "d3606135178cd644bc442f65b42ea7f1",
+    "url": "/static/media/exercises.d3606135.PNG"
+  },
+  {
+    "revision": "ab441a646500e12dec34ac487db92498",
+    "url": "/static/media/login.ab441a64.PNG"
+  },
+  {
+    "revision": "124fddcd66ca514258b99bf366bcda6e",
+    "url": "/static/media/workout.124fddcd.PNG"
+  },
+  {
+    "revision": "40b6db62c8708a15578998853a5fcc8f",
+    "url": "/static/media/workouts.40b6db62.PNG"
+  },
+  {
+    "revision": "0aa68c46ed622f676dc2a188e4f7576d",
+    "url": "/static/media/1fix.0aa68c46.png"
+  },
+  {
+    "revision": "036781428e91ad06c7e3dc920e6e9c3e",
+    "url": "/static/media/2fix.03678142.png"
+  },
+  {
+    "revision": "e071965b55c375f94da1f4e0e75a5fa8",
+    "url": "/static/media/3fix.e071965b.png"
+  },
+  {
+    "revision": "22423314b61946cf47e16884167a1660",
+    "url": "/static/media/first.22423314.png"
+  },
+  {
+    "revision": "881e9f78236e344269180855b6864641",
+    "url": "/static/media/second.881e9f78.png"
+  },
+  {
+    "revision": "2d56b45e10d2d23737fc6bc6d69ddf81",
+    "url": "/index.html"
+  }
+];
